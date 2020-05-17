f7710106c54d81103cc9486d544b649f68865efc
mac <mac@macui-MacBookAir.local>
2020-05-17 12:59:50 +0900

    Update Calendar.java main()

--- src/code/Calendar.java
+++ src/code/Calendar.java
@@ -1,9 +1,17 @@
 package code;
 
 public class Calendar {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar!");
+
+		// System.out.println -> printing out the calendar form
+		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+		System.out.println("============================");
+		System.out.println(" 1   2   3   4   5   6  7");
+		System.out.println(" 8   9  10  11  12  13  14");
+		System.out.println("15  16  17  18  19  20  21");
+		System.out.println("22  23  24  25  26  27  28");
+
 	}
 
 }
