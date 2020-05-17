c64c09760173bae447de584a3dd26bef8c030a0f
mac <mac@macui-MacBookAir.local>
2020-05-17 15:38:06 +0900

    Seperate the method and arry from the main class and execute the program just by calling the method - getMaxdaysOftheMonth(), private static final int[] MAX_DAYS

--- src/code/Calendar.java
+++ src/code/Calendar.java
@@ -1,16 +1,36 @@
 package code;
 
+import java.util.Scanner;
+
 public class Calendar {
-
-	public static void main(String[] args) {
-
-		// System.out.println -> printing out the calendar form
+	
+	private static final int[] maxdaysOftheMonth = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	public int getMaxdaysOftheMonth(int month) {
+		return maxdaysOftheMonth[month - 1];
+	}
+	
+	public static void printSampleCalendar() {
+		
 		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
 		System.out.println("============================");
 		System.out.println(" 1   2   3   4   5   6  7");
 		System.out.println(" 8   9  10  11  12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type the month : ");
+		int month = scanner.nextInt();
+		scanner.close();
+		
+		Calendar cal = new Calendar();
+		System.out.printf("The biggest dates of the month %d is %d \n", month, cal.getMaxdaysOftheMonth(month));
+		
 
 	}
 
